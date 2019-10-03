871f96019094a80956b5dc8f243693495f3add0e
Joni Iswanto <joniiswa1101@gmail.com>
2019-10-03 15:59:21 +0700

    Tugas 1-2 PHP
    Echo with table

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+echo "<h1>DAFTAR MENU</h1>";
+echo "<br>\n";
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th>No</th>";
+echo "<th>Nama Menu</th>";
+echo "<th>Harga</th>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>1</td>";
+echo "<td>Cireng</td>";
+echo "<td>Rp 1.000</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>2</td>";
+echo "<td>eS Duren</td>";
+echo "<td>Rp 15.000</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>3</td>";
+echo "<td>Es Milo</td>";
+echo "<td>Rp 20.000</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>4</td>";
+echo "<td>Es Teh</td>";
+echo "<td>Rp 5.000</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>5</td>";
+echo "<td>Piscok</td>";
+echo "<td>Rp 10.000</td>";
+echo "</tr>";
+echo "</table>";
+
+?>
